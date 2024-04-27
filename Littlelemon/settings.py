0c09b125c539441af7d40f5df884fb651148437c
Romaissa <romaissahodzic@gmail.com>
2024-04-27 18:41:12 +0100

    Django app

--- Littlelemon/settings.py
+++ Littlelemon/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'LittleLemon_Restaurant',
         'USER': 'root',
-        'PASSWORD': 'Romaissa1700150!!!',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
